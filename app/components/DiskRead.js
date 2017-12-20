ede31a67e6652a49467edb612acc20837a3c5e65
was <zxt_zel_npl@163.com>
2017-12-20 15:38:01 +0800

    DiskRead is finished

--- app/components/DiskRead.js
+++ app/components/DiskRead.js
@@ -1,63 +1,82 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 import moment from 'moment'
 import DiskItem from './DiskReadItem'
 import {public_resource} from "../constants/urls";
 import './DiskRead.less'
 
-export default class DiskRead extends React.Component{
-  constructor(props){
+export default class DiskRead extends React.Component {
+  constructor(props) {
     super(props)
+    this.checkLoading=this.checkLoading.bind(this)
   }
 
-  componentDidMount(){
-    if(this.props.disk.data.length===0){
+  componentDidMount() {
+    document.addEventListener('scroll',this.checkLoading)
+    if (this.props.disk.data.length === 0) {
       let value = {
-        limit:10,
-        query_start_stamp:0,
-        query_end_stamp:moment().format('X')
+        limit: 10,
+        query_start_stamp: 0,
+        query_end_stamp: moment().format('X')
       }
       this.props.diskActions.fetchPostsIfNeeded(value)
     }
   }
 
-  add(){
+  componentWillUnmount(){
+    document.removeEventListener('scroll',this.checkLoading)
+  }
+
+  checkLoading(){
+    let bottom_distance = parseInt(getComputedStyle(this.wrap).paddingBottom)
+    let top = this.loading.getBoundingClientRect().top
+    if(bottom_distance+top<window.innerHeight){
+      this.add()
+    }
+
+  }
+
+  add() {
     let datas = this.props.disk.data
     let len = datas.length
-    if(len>0){
-      let last = datas[len-1]
+    if (len > 0) {
+      let last = datas[len - 1]
       let query_start_stamp = 0;
       let query_end_stamp = last.timestamp
 
       let value = {
-        limit:10,
-        query_start_stamp:query_start_stamp,
-        query_end_stamp:query_end_stamp
+        limit: 5,
+        query_start_stamp: query_start_stamp,
+        query_end_stamp: query_end_stamp
       }
       this.props.diskActions.fetchPostsIfNeeded(value)
-
-      console.log(value)
     }
   }
 
-  render(){
-    let html
+  render() {
+    let htmlDom
     let datas = this.props.disk.data
-    if(datas.length===0){
-      html = (<div />)
+    let disk_img = `${public_resource}/disk.jpg`
+    let loading_img = `${public_resource}/loading.png`
+    if (datas.length === 0) {
+      htmlDom = (<div/>)
     }
-    else{
-      html = datas.map(item=>{
+    else {
+      htmlDom = datas.map(item => {
         return <DiskItem {...item} key={item.id}/>
       })
     }
 
-
     return (
-        <div>
-          DiskRead
-          {html}
-          <button onClick={this.add.bind(this)}>add</button>
+        <div className="diskRead" ref={wrap=>{this.wrap=wrap}}>
+          <p className="title">
+            <span><img src={disk_img}/>实盘解读</span>
+          </p>
+          <div>
+            {htmlDom}
+          </div>
+          <div className="loading" ref={loading=>{this.loading=loading}}>
+            <img src={loading_img} />
+          </div>
         </div>
     )
   }
