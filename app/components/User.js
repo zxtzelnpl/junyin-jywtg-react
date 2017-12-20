04c27f980b4ecd5f9681e3725e625ee6962b85a8
was <zxt_zel_npl@163.com>
2017-12-20 16:47:01 +0800

    User finished

--- app/components/User.js
+++ app/components/User.js
@@ -1,10 +1,52 @@
 import React from 'react'
+import {public_resource} from "../constants/urls";
+import './User.less'
 
-export default class User extends React.Component{
-  render(){
+export default class User extends React.Component {
+  render() {
+    let banner = `${public_resource}/userBg.jpg`
+    let user_test = `${public_resource}/user_test.jpg`
+    let serviceTime = `${public_resource}/serviceTime.jpg`
+    let productCenter = `${public_resource}/productCenter.jpg`
+    let aboutUs = `${public_resource}/aboutUs.jpg`
+    let selection_detial = `${public_resource}/selection_detail.png`
     return (
-        <div>
-          User
+        <div className="User">
+          <div className="banner">
+            <img className="bannerBg" src={banner}/>
+            <div className="userInfo">
+              <div className="circle">
+                <img src={user_test}/>
+              </div>
+              <span>13787878781</span>
+            </div>
+          </div>
+          <div className="user_selections">
+            <div className="selection">
+              <div>
+                <img className="selection_logo" src={serviceTime} alt=""/>
+                服务期限
+              </div>
+              <img className="selection_detail" src={selection_detial} alt=""/>
+            </div>
+            <div className="selection">
+              <div>
+                <img className="selection_logo" src={productCenter} alt=""/>
+                产品中心
+              </div>
+              <img className="selection_detail" src={selection_detial} alt=""/>
+            </div>
+            <div className="selection">
+              <div>
+                <img className="selection_logo" src={aboutUs} alt=""/>
+                关于我们
+              </div>
+              <img className="selection_detail" src={selection_detial} alt=""/>
+            </div>
+          </div>
+          <div className="logout">
+            <p>退出登录</p>
+          </div>
         </div>
     )
   }
