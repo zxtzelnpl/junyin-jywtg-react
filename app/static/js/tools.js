795702fae0ada3fd8c6869e43b200b0d7283562b
was <zxt_zel_npl@163.com>
2017-12-21 13:52:01 +0800

    add getQuery and getCode in tools

--- app/static/js/tools.js
+++ app/static/js/tools.js
@@ -1,3 +1,5 @@
+import {AppID} from '../../../config/keys'
+
 export function trim(str){
   return str.replace(/(^\s*)|(\s*$)/g, "");
 }
@@ -5,4 +7,20 @@ export function trim(str){
 export function phoneCheck(phone){
   const mobile = /^[1][3,4,5,7,8][0-9]{9}$/
   return mobile.test(phone)
+}
+
+export function getQuery(search) {
+  let query = {}
+  let _query = search.slice(1).split('&')
+  _query.forEach((str) => {
+    let arr = str.split('=')
+    query[arr[0]] = arr[1]
+  })
+  return query
+}
+
+export function getCode() {
+  let url = encodeURIComponent(window.location.href);
+  let urlCode = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${AppID}&redirect_uri=${url}&response_type=code&scope=snsapi_base&state=lk#wechat_redirect`
+  window.location.href = urlCode
 }
