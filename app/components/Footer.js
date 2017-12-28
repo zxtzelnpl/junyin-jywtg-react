379f71b3503de2baae2710b954249b7ea93a9ca2
was <zxt_zel_npl@163.com>
2017-12-28 10:02:30 +0800

    change the footer icons

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -15,20 +15,23 @@ import product_a from '../static/img/footer/product_a.png'
 import video from '../static/img/footer/video.png'
 import video_a from '../static/img/footer/video_a.png'
 
+const color_red = '#fc0404'
+const color_gray = '#8d8d8d'
+
 class Footer extends React.PureComponent {
   constructor(props) {
     super(props)
     this.img = {
-      main:main ,
-      main_a:main_a ,
-      disk:disk ,
-      disk_a:disk_a ,
-      news:news ,
-      news_a:news_a ,
-      product:product ,
-      product_a:product_a ,
-      video:video ,
-      video_a:video_a ,
+      main: main,
+      main_a: main_a,
+      disk: disk,
+      disk_a: disk_a,
+      news: news,
+      news_a: news_a,
+      product: product,
+      product_a: product_a,
+      video: video,
+      video_a: video_a,
       right_fix: `${public_resource}/right_fix.png`
     }
   }
@@ -54,7 +57,7 @@ class Footer extends React.PureComponent {
       alert('登录信息已经失效')
       return this.props.history.replace('/Center')
     }
-    else{
+    else {
 
     }
   }
@@ -70,53 +73,53 @@ class Footer extends React.PureComponent {
     ;
 
     let {check, receivedAt} = this.props.user
-    if(!check||moment().isAfter(moment.unix(receivedAt).add(1, 'days'))){
-      return (<div />)
+    if (!check || moment().isAfter(moment.unix(receivedAt).add(1, 'days'))) {
+      return (<div/>)
     }
 
     if (pathname === 'Main') {
       footer_main_png = this.img.main_a
-      footer_main_color = "red"
+      footer_main_color = color_red
     }
     else {
       footer_main_png = this.img.main
-      footer_main_color = "black"
+      footer_main_color = color_gray
     }
 
     if (pathname === 'VideosPage') {
       footer_user_png = this.img.video_a
-      footer_user_color = "red"
+      footer_user_color = color_red
     }
     else {
       footer_user_png = this.img.video
-      footer_user_color = "black"
+      footer_user_color = color_gray
     }
 
     if (pathname === 'DiskRead') {
       footer_disk_png = this.img.disk_a
-      footer_disk_color = "red"
+      footer_disk_color = color_red
     }
     else {
       footer_disk_png = this.img.disk
-      footer_disk_color = "black"
+      footer_disk_color = color_gray
     }
 
     if (pathname === 'NewsPage') {
       footer_news_png = this.img.news_a
-      footer_news_color = "red"
+      footer_news_color = color_red
     }
     else {
       footer_news_png = this.img.news
-      footer_news_color = "black"
+      footer_news_color = color_gray
     }
 
     if (pathname === 'ProductGameMaster') {
       footer_product_png = this.img.product_a
-      footer_product_color = "red"
+      footer_product_color = color_red
     }
     else {
       footer_product_png = this.img.product
-      footer_product_color = "black"
+      footer_product_color = color_gray
     }
 
     if (pathname === "Center") {
@@ -127,34 +130,24 @@ class Footer extends React.PureComponent {
         <div>
           <footer>
             <Link to="/Main">
-              <div>
-                <img src={footer_main_png} alt=""/>
-                <span style={{"color": footer_main_color}}>首页</span>
-              </div>
+              <img src={footer_main_png} alt=""/>
+              <span style={{"color": footer_main_color}}>首页</span>
             </Link>
             <Link to="/DiskRead">
-              <div>
-                <img src={footer_disk_png} alt=""/>
-                <span style={{"color": footer_disk_color}}>解盘</span>
-              </div>
+              <img src={footer_disk_png} alt=""/>
+              <span style={{"color": footer_disk_color}}>解盘</span>
             </Link>
             <Link to="/ProductGameMaster">
-              <div>
-                <img src={footer_product_png} alt=""/>
-                <span style={{"color": footer_product_color}}>微投顾</span>
-              </div>
+              <img src={footer_product_png} alt=""/>
+              <span style={{"color": footer_product_color}}>微投顾</span>
             </Link>
             <Link to="/VideosPage">
-              <div>
-                <img src={footer_user_png} alt=""/>
-                <span style={{"color": footer_user_color}}>视频</span>
-              </div>
+              <img src={footer_user_png} alt=""/>
+              <span style={{"color": footer_user_color}}>视频</span>
             </Link>
             <Link to="/NewsPage">
-              <div>
-                <img src={footer_news_png} alt=""/>
-                <span style={{"color": footer_news_color}}>资讯</span>
-              </div>
+              <img src={footer_news_png} alt=""/>
+              <span style={{"color": footer_news_color}}>资讯</span>
             </Link>
           </footer>
 
--- needToDo.md
+++ needToDo.md
@@ -1,2 +1,5 @@
 1.所有图标需要更换
 2.视频看看老师的是否有传错的
+3.http://iconfont.cn/collections/detail?spm=a313x.7781069.1998910419.d9df05512&cid=6250（icon网址）
+8d8d8d
+fc0404
