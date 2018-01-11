5c1f722076817c675bc7cb2490ea45420dd81a2b
was <zxt_zel_npl@163.com>
2018-01-11 15:45:53 +0800

    add a little file

--- app/components/MyVideo.js
+++ app/components/MyVideo.js
@@ -4,17 +4,18 @@ import {isAndroid, x5Enter, x5Exit} from "../static/js/tools"
 export default class MyVideo extends React.Component {
   constructor(props) {
     super(props)
+    this.isAndroid = isAndroid()
   }
 
   componentDidMount() {
-    if(isAndroid()){
+    if(this.isAndroid){
       this.video.addEventListener('x5videoenterfullscreen', x5Enter)
       this.video.addEventListener('x5videoexitfullscreen', x5Exit)
     }
   }
 
   componentWillUnmount() {
-    if(isAndroid()){
+    if(this.isAndroid){
       this.video.removeEventListener('x5videoenterfullscreen', x5Enter)
       this.video.removeEventListener('x5videoexitfullscreen', x5Exit)
     }
@@ -22,20 +23,39 @@ export default class MyVideo extends React.Component {
 
   render() {
     let {src,poster} = this.props
-    return(
-        <video
-            src={src}
-            poster={poster}
-            controls="false"
-            style={{
-              'objectFit': 'contain'
-            }}
-            plays-inline="true"
-            webkit-playsinline="true"
-            x5-video-player-type="h5"
-            x5-video-player-fullscreen="true"
-            ref={video=>{this.video=video}}
-        >您的设备暂不支持此视频</video>
-    )
+    if(this.isAndroid){
+      return(
+          <video
+              src={src}
+              poster={poster}
+              style={{
+                'objectFit': 'contain'
+              }}
+              plays-inline="true"
+              webkit-playsinline="true"
+              x5-video-player-type="h5"
+              x5-video-player-fullscreen="true"
+              ref={video=>{this.video=video}}
+          >您的设备暂不支持此视频</video>
+      )
+    }
+    else{
+      return(
+          <video
+              src={src}
+              poster={poster}
+              controls="false"
+              style={{
+                'objectFit': 'contain'
+              }}
+              plays-inline="true"
+              webkit-playsinline="true"
+              x5-video-player-type="h5"
+              x5-video-player-fullscreen="true"
+              ref={video=>{this.video=video}}
+          >您的设备暂不支持此视频</video>
+      )
+    }
+
   }
 }
