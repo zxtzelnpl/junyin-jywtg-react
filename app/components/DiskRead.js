4eb90552bb745b6d4998cc3dd2e7741b78dc3c96
was <zxt_zel_npl@163.com>
2018-01-12 17:03:07 +0800

    loadControl begin

--- app/components/DiskRead.js
+++ app/components/DiskRead.js
@@ -2,16 +2,15 @@ import './DiskRead.less'
 import React from 'react'
 import moment from 'moment'
 import DiskItem from './DiskReadItem'
-import {public_resource} from "../constants/urls";
+import LoadControl from './LoadControl'
 
 export default class DiskRead extends React.PureComponent {
   constructor(props) {
     super(props)
-    this.checkLoading=this.checkLoading.bind(this)
+    this.add=this.add.bind(this)
   }
 
   componentDidMount() {
-    document.addEventListener('scroll',this.checkLoading)
     if (this.props.disk.data.length === 0) {
       let value = {
         limit: 10,
@@ -20,22 +19,6 @@ export default class DiskRead extends React.PureComponent {
       }
       this.props.diskActions.fetchPostsIfNeeded(value)
     }
-  }
-
-  componentWillUnmount(){
-    document.removeEventListener('scroll',this.checkLoading)
-    clearTimeout(this.timeId)
-  }
-
-  checkLoading(){
-    clearTimeout(this.timeId)
-    this.timeId = setTimeout(()=>{
-      let bottom_distance = parseInt(getComputedStyle(this.wrap).paddingBottom)
-      let top = this.loading.getBoundingClientRect().top
-      if(bottom_distance+top<window.innerHeight){
-        this.add()
-      }
-    },100)
   }
 
   add() {
@@ -58,8 +41,6 @@ export default class DiskRead extends React.PureComponent {
   render() {
     let htmlDom
     let datas = this.props.disk.data
-    let head_img = `${public_resource}/disk_read_head.jpg`
-    let loading_img = `${public_resource}/loading.png`
     if (datas.length === 0) {
       htmlDom = (<div/>)
     }
@@ -70,7 +51,7 @@ export default class DiskRead extends React.PureComponent {
     }
 
     return (
-        <div className="diskRead" ref={wrap=>{this.wrap=wrap}}>
+        <div className="diskRead">
           <div className="title">
             实盘解读
           </div>
@@ -78,9 +59,7 @@ export default class DiskRead extends React.PureComponent {
             <div>
               {htmlDom}
             </div>
-            <div className="loading" ref={loading=>{this.loading=loading}}>
-              <img src={loading_img} />
-            </div>
+            <LoadControl isFetching={this.props.disk.isFetching} loadFunction={this.add}/>
           </div>
         </div>
     )
--- app/components/LoadControl.js
+++ app/components/LoadControl.js
@@ -1,7 +1,45 @@
 'use strict'
-
+import './LoadControl.less'
 import React from 'react'
+import {public_resource} from "../constants/urls";
 
-export default class LoadControl extends React.Component{
+export default class LoadControl extends React.Component {
+  constructor (props) {
+    super(props)
+    this.timeId = null
+    this.check = this.check.bind(this)
+  }
 
+  componentDidMount () {
+    window.addEventListener('scroll', this.check)
+  }
+
+  componentWillUnmount () {
+    clearTimeout(this.timeId)
+    window.removeEventListener('scroll', this.check)
+  }
+
+  check () {
+    clearTimeout(this.timeId)
+    this.timeId = setTimeout(() => {
+      let top = this.load.getBoundingClientRect().top
+      let height = window.innerHeight
+      if (top <= height-50) {
+        this.props.loadFunction()
+      }
+    }, 200)
+  }
+
+  render () {
+    console.log(this.props.isFetching)
+    let loading_img = `${public_resource}/loading.png`
+    let visible = this.props.isFetching ? 'visible' : 'hidden'
+    return (
+        <div className="LoadControl"
+             ref={load => {this.load = load }}
+             style={{visibility: visible}}>
+          <img src={loading_img}/>
+        </div>
+    )
+  }
 }
