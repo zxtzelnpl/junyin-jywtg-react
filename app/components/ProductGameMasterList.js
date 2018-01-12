ac0f5a4ede6fbe7c33f574172f03e8a3901ea9b7
was <zxt_zel_npl@163.com>
2018-01-12 17:17:41 +0800

    loadControl has cover 3

--- app/components/ProductGameMasterList.js
+++ app/components/ProductGameMasterList.js
@@ -17,11 +17,17 @@ export default class ProductGameMasterList extends React.PureComponent {
     let htmlDom = <div>数据正在加载中</div>
     let data = this.props.record.data
     if(data.length>0){
-      htmlDom = data.reverse().map(item=>{
+      htmlDom = data.reverse().map((item,index)=>{
+        let url
         let {Record_url,Createtime} = item
-        let url = `http://${Record_url}`
+        if(Record_url.indexOf('http')>-1){
+          url = Record_url
+        }
+        else{
+          url = `http://${Record_url}`
+        }
         return (
-            <a href={url}>
+            <a href={url} key={index}>
               <p className="record_title">博弈大师</p>
               <span className="record_time">{moment(Createtime).format('YYYY-MM-DD HH:mm')}</span>
             </a>
