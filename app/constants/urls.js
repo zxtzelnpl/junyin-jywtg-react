853043da3087262f12927a44a5a5d0774b435f1d
was <zxt_zel_npl@163.com>
2018-03-06 15:45:55 +0800

    add notes for apis

--- app/constants/urls.js
+++ app/constants/urls.js
@@ -1,10 +1,10 @@
 export const disk_read = 'http://guanwei.zgjy518.com/Disk_read/ec595eaeec302e8b'
 export const news_infomation='http://guanwei.zgjy518.com/News_information/ae84df3418730470'
 export const teacher_report ='http://guanwei.zgjy518.com/Teacher_report/b9f3f290bbd38c3ab'
-export const user_check='/ashx/User_Login.ashx'
-export const public_resource = 'https://public.jyzqsh.com:447/jywtg'
-export const user_record = '/ashx/user_Record.ashx'
 export const exchange_guide='http://guanwei.zgjy518.com/video/a1e9d77e95b5cf993'
 export const special_class='http://guanwei.zgjy518.com/video/b5a2661f6a2b763e'
-export const Article_browse_Record= '/ashx/Article_browse_Record.ashx'
-export const Video_browse_Record= '/ashx/Video_browse_Record.ashx'
+export const public_resource = 'https://public.jyzqsh.com:447/jywtg'//静态图片资源
+export const user_check='/ashx/User_Login.ashx'//用户登录
+export const user_record = '/ashx/user_Record.ashx'//用户发送模版消息记录
+export const Article_browse_Record= '/ashx/Article_browse_Record.ashx'//文章点击记录
+export const Video_browse_Record= '/ashx/Video_browse_Record.ashx'//视频点击观看记录
